4f3c4591841be8ced075701b8154f7114f5f262f
Shaye Baisac <shayebaisac@gmail.com>
2022-04-16 23:06:19 +0800

    [3] Use vue method

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
             courseGoal: 'Finish the course and learn vue!',
             vueLink: 'https://vuejs.org/'
         };
+    },
+    methods: {
+        outputGoal() {
+            const randomNum = Math.random();
+            if (randomNum < 0.5) {
+                return 'Learn Vue!';
+            } else {
+                return 'Learn React too!';
+            }
+        }
     }
 });
 
