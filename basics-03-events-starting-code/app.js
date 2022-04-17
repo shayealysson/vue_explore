e4c24c4fff822174ec02f3a1a65bb3f88ded8f44
Shaye Baisac <shayebaisac@gmail.com>
2022-04-17 19:47:58 +0800

    [1] Using v-on:click event

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -4,6 +4,14 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  methods: {
+    increaseCounter() {
+        return this.counter++;
+    },
+    decreaseCounter() {
+      return this.counter--;
+  }
+}
 });
 
 app.mount('#events');
