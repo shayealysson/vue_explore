5f22cb2cf49bdcedc8d83cd2cdd8fbc1d0fe9ba1
Shaye Baisac <shayebaisac@gmail.com>
2022-04-17 23:58:35 +0800

    [1] Show an alert (any text of your choice) when the button is pressed

---
+++ basics-assignment-2-problem/app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+    },
+    methods: {
+        displayAlert() {
+            alert('I pressed the button!');
+        },
+    }
+});
+
+app.mount('#assignment');
