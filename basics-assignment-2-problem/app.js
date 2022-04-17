a609dbc3bd477680f415c9e9c59c9feed2489bdf
Shaye Baisac <shayebaisac@gmail.com>
2022-04-18 00:10:59 +0800

    [2] Register the user input on keydown and output it in the paragraph

--- basics-assignment-2-problem/app.js
+++ basics-assignment-2-problem/app.js
@@ -1,10 +1,16 @@
 const app = Vue.createApp({
     data() {
+        return {
+            input1: ''
+        };
     },
     methods: {
         displayAlert() {
             alert('I pressed the button!');
         },
+        setOutput(event) {
+            this.input1 = event.target.value;
+        }
     }
 });
 
