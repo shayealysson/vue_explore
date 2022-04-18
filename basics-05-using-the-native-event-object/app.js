5c9e4e7dcea176f2107c99903a06c9c9d0c356bb
Shaye Baisac <shayebaisac@gmail.com>
2022-04-18 23:27:13 +0800

    [6] Good example for when to use watcher over computed property

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -3,33 +3,42 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: '',
-      fullName: ''
     };
   },
   watch: {
+    //will not be executed on initaialized, only when depency data is updated
+    /*
     name(value) {
       console.log('Running again...');
       if (value == '')
         this.fullName = '';
       else
         this.fullName = value + ' ' + 'III';
+    }*/
+    counter(value) {
+      console.log('Running counter watcher...');
+      if(value > 15) {
+        this.counter = 0;
+      }
     }
+
   },
   computed: {
-    /*
+    //will be executed once on initialize
     fullName(){
       console.log('Running again...');
       if (this.name == '')
         return '';
       else
         return this.name + ' ' + 'III';
-    },*/
+    },
   },
   methods: {
     setName(event) {
       this.name = event.target.value;
     },
     add(num) {
+      console.log('Running add method...');
       this.counter = this.counter + num;
     },
     reduce(num) {
